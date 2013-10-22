a892c22f2590dd962df591dadc3fcfd56c405110
Christian Foellmann <foellmann@foe-services.de>
2013-10-22 22:31:05 +0200

    added localization for demo

--- includes/demo.php
+++ includes/demo.php
@@ -13,19 +13,19 @@
 		<thead>
 		<tr>
 			<th>
-				First Name
+				<?php _e( 'First Name', 'footable' ); ?>
 			</th>
 			<th data-hide="phone">
-				Last Name
+				<?php _e( 'Last Name', 'footable' ); ?>
 			</th>
 			<th data-hide="phone,tablet">
-				Job Title
+				<?php _e( 'Job Title', 'footable' ); ?>
 			</th>
 			<th data-hide="phone,tablet">
-				DOB
+				<?php _e( 'DOB', 'footable' ); ?>
 			</th>
 			<th data-hide="phone">
-				Status
+				<?php _e( 'Status', 'footable' ); ?>
 			</th>
 		</tr>
 		</thead>
@@ -35,70 +35,70 @@
 			<td><a href="#">Boudreaux</a></td>
 			<td>Traffic Court Referee</td>
 			<td data-value="78025368997">22 Jun 1972</td>
-			<td data-value="1"><span class="status-metro status-active" title="Active">Active</span></td>
+			<td data-value="1"><span class="status-metro status-active" title="<?php _e( 'Active', 'footable' ); ?>"><?php _e( 'Active', 'footable' ); ?></span></td>
 		</tr>
 		<tr>
 			<td>Shona</td>
 			<td>Woldt</td>
 			<td><a href="#">Airline Transport Pilot</a></td>
 			<td data-value="370961043292">3 Oct 1981</td>
-			<td data-value="2"><span class="status-metro status-disabled" title="Disabled">Disabled</span></td>
+			<td data-value="2"><span class="status-metro status-disabled" title="<?php _e( 'Disabled', 'footable' ); ?>"><?php _e( 'Disabled', 'footable' ); ?></span></td>
 		</tr>
 		<tr>
 			<td>Granville</td>
 			<td>Leonardo</td>
 			<td>Business Services Sales Representative</td>
 			<td data-value="-22133780420">19 Apr 1969</td>
-			<td data-value="3"><span class="status-metro status-suspended" title="Suspended">Suspended</span></td>
+			<td data-value="3"><span class="status-metro status-suspended" title="<?php _e( 'Suspended', 'footable' ); ?>"><?php _e( 'Suspended', 'footable' ); ?></span></td>
 		</tr>
 		<tr>
 			<td>Easer</td>
 			<td>Dragoo</td>
 			<td>Drywall Stripper</td>
 			<td data-value="250833505574">13 Dec 1977</td>
-			<td data-value="1"><span class="status-metro status-active" title="Active">Active</span></td>
+			<td data-value="1"><span class="status-metro status-active" title="<?php _e( 'Active', 'footable' ); ?>"><?php _e( 'Active', 'footable' ); ?></span></td>
 		</tr>
 		<tr>
 			<td>Maple</td>
 			<td>Halladay</td>
 			<td>Aviation Tactical Readiness Officer</td>
 			<td data-value="694116650726">30 Dec 1991</td>
-			<td data-value="3"><span class="status-metro status-suspended" title="Suspended">Suspended</span></td>
+			<td data-value="3"><span class="status-metro status-suspended" title="<?php _e( 'Suspended', 'footable' ); ?>"><?php _e( 'Suspended', 'footable' ); ?></span></td>
 		</tr>
 		<tr>
 			<td>Maxine</td>
 			<td><a href="#">Woldt</a></td>
 			<td><a href="#">Business Services Sales Representative</a></td>
 			<td data-value="561440464855">17 Oct 1987</td>
-			<td data-value="2"><span class="status-metro status-disabled" title="Disabled">Disabled</span></td>
+			<td data-value="2"><span class="status-metro status-disabled" title="<?php _e( 'Disabled', 'footable' ); ?>"><?php _e( 'Disabled', 'footable' ); ?></span></td>
 		</tr>
 		<tr>
 			<td>Lorraine</td>
 			<td>Mcgaughy</td>
 			<td>Hemodialysis Technician</td>
 			<td data-value="437400551390">11 Nov 1983</td>
-			<td data-value="2"><span class="status-metro status-disabled" title="Disabled">Disabled</span></td>
+			<td data-value="2"><span class="status-metro status-disabled" title="<?php _e( 'Disabled', 'footable' ); ?>"><?php _e( 'Disabled', 'footable' ); ?></span></td>
 		</tr>
 		<tr>
 			<td>Lizzee</td>
 			<td><a href="#">Goodlow</a></td>
 			<td>Technical Services Librarian</td>
 			<td data-value="-257733999319">1 Nov 1961</td>
-			<td data-value="3"><span class="status-metro status-suspended" title="Suspended">Suspended</span></td>
+			<td data-value="3"><span class="status-metro status-suspended" title="<?php _e( 'Suspended', 'footable' ); ?>"><?php _e( 'Suspended', 'footable' ); ?></span></td>
 		</tr>
 		<tr>
 			<td>Judi</td>
 			<td>Badgett</td>
 			<td>Electrical Lineworker</td>
 			<td data-value="362134712000">23 Jun 1981</td>
-			<td data-value="1"><span class="status-metro status-active" title="Active">Active</span></td>
+			<td data-value="1"><span class="status-metro status-active" title="<?php _e( 'Active', 'footable' ); ?>"><?php _e( 'Active', 'footable' ); ?></span></td>
 		</tr>
 		<tr>
 			<td>Lauri</td>
 			<td>Hyland</td>
 			<td>Blackjack Supervisor</td>
 			<td data-value="500874333932">15 Nov 1985</td>
-			<td data-value="3"><span class="status-metro status-suspended" title="Suspended">Suspended</span></td>
+			<td data-value="3"><span class="status-metro status-suspended" title="<?php _e( 'Suspended', 'footable' ); ?>"><?php _e( 'Suspended', 'footable' ); ?></span></td>
 		</tr>
 		</tbody>
 	</table>
